5c3ed13ed204997b2f9bcd6711ed0b98c5a42c49
Dmytro Habaznia <karambolrul@gmail.com>
2024-05-14 02:16:17 +0300

    Added containerization

--- back-end/src/lib/images/index.js
+++ back-end/src/lib/images/index.js
@@ -1,5 +1,6 @@
 import {create as createImageInDb, getByName} from "../../repositories/images/index.js";
 import {getFileExtension, getFilenameWithoutExtension, isImage} from "../common/index.js";
+import * as crypto from "crypto";
 
 export async function createImage(file) {
     if (!isImage(file)) {
