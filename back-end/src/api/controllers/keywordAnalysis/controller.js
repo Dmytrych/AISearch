bbc171fffba346a85b2a6410fdb6823d3e776f3d
Dmytro Habaznia <55029343+Dmytrych@users.noreply.github.com>
2024-06-02 16:04:10 +0300

    Fixed keyword generation

--- back-end/src/api/controllers/keywordAnalysis/controller.js
+++ back-end/src/api/controllers/keywordAnalysis/controller.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getKeywords(req, res) {
-  const response = await axios.post('http://127.0.0.1:5000/analyze', req.body, {
+  const response = await axios.post(`${process.env.KEYWORD_ANALYZER_URL}/analyze`, req.body, {
     headers: {
       'Content-Type': 'application/json'
     }
